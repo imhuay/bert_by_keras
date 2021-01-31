4d4a1045a2efeac03c16eba17f383680f0b25d53
imhuay <imhuay@163.com>
2021-01-31 15:05:57 +0800

    Squashed 'code/keras_utils/' content from commit a490b57
    git-subtree-dir: code/keras_utils
    git-subtree-split: a490b57d80829bde05c5fcc2d5752b7657a79b18

---
+++ README.md
@@ -0,0 +1,9 @@
+keras_demo
+===
+
+简介
+---
+一些自定义 keras 代码
+
+### 相关仓库
+- [imhuay/bert_by_keras](https://github.com/imhuay/bert_by_keras)
---
+++ callback_keras/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+Time:
+    2021-01-26 19:32
+    
+Author:
+    huayang
+    
+Subject:
+    
+"""
