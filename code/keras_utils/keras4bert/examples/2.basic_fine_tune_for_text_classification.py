50787c2e09a878978d1d64ed0f24bd15d1c40f6e
imhuay <imhuay@163.com>
2021-02-01 16:47:33 +0800

    完善示例代码

--- code/keras_utils/keras4bert/examples/2.basic_fine_tune_for_text_classification.py
+++ code/keras_utils/keras4bert/examples/2.basic_fine_tune_for_text_classification.py
@@ -18,14 +18,15 @@ except:
     import keras
     import keras.backend as K
 
-from bert_keras.model import build_bret_from_config
+from bert_keras.model import build_bret, model_fine_tune_config
 
 
 def build_model(config_path, checkpoint_path, sequence_len, n_class):
     """"""
-    bert = build_bret_from_config(config_path, checkpoint_path,
+    bert, layer_name = build_bret(config_path, checkpoint_path,
                                   sequence_len=sequence_len,
-                                  return_type='cls_embedding')  # 设置模型返回 cls_embedding
+                                  output_type='cls_embedding',
+                                  return_layer_name=True)  # 设置模型返回 cls_embedding
     # 获取 [CLS] 向量
     x = bert.output  # 这里用 output，outputs 返回的是一个列表
     x = keras.layers.Dropout(0.1, name='Classification_Dropout')(x)  # 如果用 outputs，这里要传 x[0]
@@ -33,7 +34,7 @@ def build_model(config_path, checkpoint_path, sequence_len, n_class):
     outputs = keras.layers.Dense(n_class, name='Classification_Softmax')(x)
 
     model = keras.Model(bert.inputs, outputs, name='Bert_Classification')
-    return model
+    return model, layer_name
 
 
 def main():
@@ -44,15 +45,29 @@ def main():
 
     n_epoch = 5
     sequence_len = 128
-    n_class = 8
+    n_class = 2
 
-    model = build_model(config_path, checkpoint_path, sequence_len, n_class)
+    model, layer_name = build_model(config_path, checkpoint_path, sequence_len, n_class)
     model.summary(line_length=200)
 
-    model.compile(
-        loss='sparse_categorical_crossentropy',
-        optimizer=keras.optimizers.Adam(1e-5),  # 使用足够小的学习率，建议 1e-5 ~ 5e-5
-        metrics=['accuracy'])
+    # 控制微调哪些层
+    model = model_fine_tune_config(
+        model, layer_name,
+        is_fine_tune=True,
+        is_fine_tune_norm=False,  # layer norm 不参与微调
+        is_fine_tune_embedding=False,  # embedding 不参与微调
+        n_fine_tune_transformer_block=3  # 只微调 3 个 transformer_block（接近输出层的 block）
+    )
+
+    # 打印参与微调的层
+    print('\n===== 参与 fine tune 的层 =====\n')
+    for layer in model.layers:
+        if layer.trainable_weights:  # 使用 trainable 判断会把所有层都打印出来，不知道为什么
+            print(layer.name)
+
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer=keras.optimizers.Adam(1e-5),  # 使用足够小的学习率，建议 1e-5 ~ 5e-5
+                  metrics=['accuracy'])
 
     # TODO: 准备数据
     # model.fit(ds_train, epochs=n_epoch, verbose=1)
